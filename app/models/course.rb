aae4f1e8a9ead8e2a09f60da7d7b181c5f91c348
Vijay Kumar Badugu <badugu.vijay@gmail.com>
2016-02-14 19:10:18 -0500

    edit course done

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,10 @@ class Course < ActiveRecord::Base
   has_many :course_instructors
   has_many :instructors
   has_many :coursepage_materials
+  validate :DateChecker
+  def DateChecker
+    if start_date > end_date || start_date < Date.today
+      errors.add("Dates are not selected properly")
+    end
+  end
 end
