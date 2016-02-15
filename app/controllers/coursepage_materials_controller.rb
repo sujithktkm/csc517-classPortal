4bd41d06d6cd93f72d220f234c54c8923f47104c
ashendg <ashendg@ncsu.edu>
2016-02-14 21:35:34 -0500

    Add fix the mytype issue

--- app/controllers/coursepage_materials_controller.rb
+++ app/controllers/coursepage_materials_controller.rb
@@ -7,17 +7,17 @@ class CoursepageMaterialsController < ApplicationController
     @coursepage_material = CoursepageMaterial.new(materials_params)
     if @coursepage_material.save
       # session[:material_id] = @student.id
-      flash[:notice] = "Login successful"
+      flash[:success] = "Added successfully"
       redirect_to '/courses/show'
     else
-      flash[:notice] = "Cannot login. Please try again"
+      flash[:danger] = "Cannot Add. Please contact admin"
       redirect_to '/coursepage_materials/create'
     end
   end
 
   private
   def materials_params
-    params.require(:coursepage_material).permit(:title, :description, :type, :course_id)
+    params.require(:coursepage_material).permit(:title, :description, :material_type, :course_id)
   end
 
 end
