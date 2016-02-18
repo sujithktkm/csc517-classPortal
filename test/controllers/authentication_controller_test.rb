0176dd308fb617899c06b136ef8a4b27a9a0faa4
sujithktkm <skataka@ncsu.edu>
2016-02-18 02:50:41 -0500

    Tests added

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -5,32 +5,25 @@ class AuthenticationControllerTest < ActionController::TestCase
   #   assert true
   # end
 
-  # test 'User login' do
-  #   # login_user = users :one
-  #   post :create, session: {email: 'test@test.com', password: 'test', name: 'name', type: 'Student' }
-  #   assert_equal login_user.id, session[:user_id]
-  #   assert_redirected_to root_path
-  # end
-  #
-  # test 'Validate email' do
-  #   post :create, session: {email: nil, password: 'password'}
-  #   assert_nil session[:user_id]
-  #   assert_redirected_to login_path
-  # end
-  #
-  # test 'Validate password' do
-  #   user = users :one
-  #   post :create, session: {email: user.email, password: nil}
-  #   assert_nil session[:user_id]
-  #   assert_redirected_to login_path
-  # end
-  #
-  # test 'Can user logout' do
-  #   login_user = users :one
-  #   session[:user_id] = login_user.id
-  #   delete :destroy
-  #   assert_nil session[:user_id]
-  #   assert_redirected_to login_path
-  # end
+  test 'User login' do
+    login_user = users :one
+    post :create, session: {email: 'email@email.com', password: 'password', name: 'name', type: 'Student' }
+    assert_equal login_user.id, session[:user_id]
+    assert_redirected_to root_path
+  end
+
+  test 'Validate fields' do
+    post :create, session: {email: nil, password: nil}
+    assert_nil session[:user_id]
+    assert_redirected_to login_path
+  end
+
+  test 'Can user logout' do
+    login_user = users :one
+    session[:user_id] = login_user.id
+    delete :destroy
+    assert_nil session[:user_id]
+    assert_redirected_to login_path
+  end
 
 end
