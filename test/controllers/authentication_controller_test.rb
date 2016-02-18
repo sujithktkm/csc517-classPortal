7bb0cbca7cb68b101f177b0caff1956882066cb1
sujithktkm <skataka@ncsu.edu>
2016-02-18 02:29:18 -0500

    Tests started

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -4,4 +4,33 @@ class AuthenticationControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test 'User login' do
+  #   # login_user = users :one
+  #   post :create, session: {email: 'test@test.com', password: 'test', name: 'name', type: 'Student' }
+  #   assert_equal login_user.id, session[:user_id]
+  #   assert_redirected_to root_path
+  # end
+  #
+  # test 'Validate email' do
+  #   post :create, session: {email: nil, password: 'password'}
+  #   assert_nil session[:user_id]
+  #   assert_redirected_to login_path
+  # end
+  #
+  # test 'Validate password' do
+  #   user = users :one
+  #   post :create, session: {email: user.email, password: nil}
+  #   assert_nil session[:user_id]
+  #   assert_redirected_to login_path
+  # end
+  #
+  # test 'Can user logout' do
+  #   login_user = users :one
+  #   session[:user_id] = login_user.id
+  #   delete :destroy
+  #   assert_nil session[:user_id]
+  #   assert_redirected_to login_path
+  # end
+
 end
--- test/controllers/instructors_controller_test.rb
+++ test/controllers/instructors_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class InstructorsControllerTest < ActionController::TestCase
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get :edit
+  #   assert_response :success
+  # end
+  #
+  # test "should get update" do
+  #   get :update
+  #   assert_response :success
+  # end
 
 end
